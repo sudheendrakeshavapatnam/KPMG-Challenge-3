6c87eaaeaad32530b67890cf947651b4069f8819
Sudheendra Keshavapatnam <Sudheendra.keshavapatnam@gmail.com>
2022-09-07 10:33:54 +0530

    Challenge3 Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Challenge-3
+
+We have a nested object, we would like a function that you pass in the object and a key and get back the value. How this is implemented is up to you.
+Example Inputs
+object = {“a”:{“b”:{“c”:”d”}}}
+key = a/b/c
+object = {“x”:{“y”:{“z”:”a”}}}
+key = x/y/z
+value = a
+
+
+![image](https://user-images.githubusercontent.com/90386933/132696383-ae1b5243-83d7-470f-a21e-833aef4be551.png)
---
+++ challenge3.ps1
@@ -0,0 +1,29 @@
+﻿$object ='{
+		   "a": {
+		          "b": { 
+		    	       "c": "d"          
+						} 
+		        }    
+		 }'| ConvertFrom-Json 
+
+[string]$key;    
+
+function getobjVal($key){
+$array = $key.Split("/")    
+$tempobject = $object    
+for ($i=0; $i -lt $array.Length; $i++){           
+$temp= $array[$i]    
+$tempobject =  $tempobject.$temp    
+
+} 
+$tempobject
+}
+
+#Output
+####PS C:> getobjVal('a/b');
+####
+####   c
+####   -
+####   d
+#PS C:> getobjVal('a/b/c');
+#d
